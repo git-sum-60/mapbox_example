97050c2a7a225bf0f4c2af9a970733434b1569ba
brian <riceb53@gmail.com>
2020-04-26 12:34:50 -0400

    add mapbox code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div id="map"></div>
   </div>
 </template>
 
+<style>
+	body { margin: 0; padding: 0; }
+	#map { position: absolute; top: 0; bottom: 0; width: 100%; height: 50% }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      places: [
+        {}
+      ]
+    };
+  },
+  mounted: function() {
+    mapboxgl.accessToken = '';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    center: [12.550343, 55.665957],
+    zoom: 5,
+    style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+    var marker = new mapboxgl.Marker()
+    .setLngLat([12.550343, 55.665957])
+    .addTo(map);
+
+    var marker = new mapboxgl.Marker()
+    .setLngLat([-78.8784, 42.8864])
+    .addTo(map);
+
+
+  },
+  methods: {}
+};
 </script>
+
+
